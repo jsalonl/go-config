97547321e1e4b11da9208c543c90769824f6fcea
jsalonl <joansalomon@gmail.com>
2024-09-06 01:57:43 -0500

    feat: Update to V2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,8 +5,45 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [v2.0.0] - 2024-09-06
+
+### Changed
+
+- Refactored `GoConfig` to use an interface and `goConfig` implementation.
+- Added `unmarshallFunc` parameter to `NewGoConfig` to allow custom unmarshalling functions.
+- Renamed `NewConfig` to `ParseConfig` and `readConfig` to `read` for clarity.
+- Updated error handling to use `fmt.Errorf` with the new `formatError` constant.
+- Improved regex patterns for environment variables and `.env` file parsing.
+- Simplified environment variable replacement logic in `replaceEnvVariables`.
+- Updated `unmarshallYAML` to handle errors using the new error format.
+
+### Removed
+
+- Deprecated `ErrVariableNotFound`, `ErrReadingFile`, `ErrUnmarshalling`, `ErrUnsupportedExt`, `ErrReadingConfig`,
+- `ErrOpeningEnvFile`, and `ErrInvalidEnvFormat` constants in favor of a consolidated error handling approach.
+
+### Fixed
+
+- Corrected the error message formatting in `openFile` and `read`.
+- Improved the handling of unsupported file extensions in the `read` function.
+- Enhanced the `.env` file parsing to handle invalid formats more gracefully.
+
+### Other Changes
+
+- **Error Handling:** All error constants are now defined using the `errors` package. This change consolidates error
+  messages and simplifies error management across the package. The following errors are now managed via `errors.New`:
+  - `ErrUnmarshalling`
+  - `ErrVariableNotFound`
+  - `ErrUnsupportedExt`
+  - `ErrReadingFile`
+  - `ErrOpenDir`
+  - `ErrOpeningEnvFile`
+  - `ErrInvalidEnvFormat`
+
 ## [v1.0.0] - 2024-08-27
+
 ### Added
+
 - Initial release of `GoConfig`.
 - Support for reading YAML and JSON configuration files.
 - Environment variable substitution in configuration files.
--- goconfig/config_test.go
+++ goconfig/config_test.go
@@ -1,120 +1,254 @@
-package goconfig
+package goconfig_test
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/jsalonl/go-config/v2/goconfig"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 const (
 	configFileYaml = "app.yaml"
-	configFileJson = "app.json"
 )
 
-func TestNewConfigSuccessYAML(t *testing.T) {
-	dir := t.TempDir()
-	content := `name: TestApp
-version: 1.0`
-	err := os.WriteFile(filepath.Join(dir, configFileYaml), []byte(content), 0644)
-	assert.NoError(t, err)
-	var yamlCfg struct {
-		Name    string `yaml:"name"`
-		Version string `yaml:"version"`
+func TestParseConfig(t *testing.T) {
+	config := goconfig.NewGoConfig()
+
+	assert.NotNil(t, config)
+}
+
+func TestParseConfigWithCustomUnmarshall(t *testing.T) {
+	customUnmarshall := func(structure interface{}, content []byte) error {
+		return nil
 	}
-	err = NewConfig(&yamlCfg, "app", dir)
-	assert.NoError(t, err)
-	assert.Equal(t, "TestApp", yamlCfg.Name)
-	assert.Equal(t, "1.0", yamlCfg.Version)
+	config := goconfig.NewGoConfig(customUnmarshall)
+
+	assert.NotNil(t, config)
 }
 
-func TestNewConfigSuccessMultipleDir(t *testing.T) {
-	dir := t.TempDir()
-	content := `name: TestApp
-version: 1.0`
-	err := os.WriteFile(filepath.Join(dir, configFileYaml), []byte(content), 0644)
+func TestLoadEnvSuccess(t *testing.T) {
+	content := `APP_NAME=TestApp
+APP_VERSION=1.0
+`
+	createEnvFile(t, content)
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	err := config.LoadEnv()
 	assert.NoError(t, err)
-	var yamlCfg struct {
-		Name    string `yaml:"name"`
-		Version string `yaml:"version"`
-	}
-	err = NewConfig(&yamlCfg, "app", dir, "configuration")
-	assert.NoError(t, err)
-	assert.Equal(t, "TestApp", yamlCfg.Name)
-	assert.Equal(t, "1.0", yamlCfg.Version)
+
+	assert.Equal(t, "TestApp", os.Getenv("APP_NAME"))
+	assert.Equal(t, "1.0", os.Getenv("APP_VERSION"))
+
+	_ = os.Unsetenv("APP_NAME")
+	_ = os.Unsetenv("APP_VERSION")
+	removeEnvFile(t)
 }
 
-func TestNewConfigSuccessJSON(t *testing.T) {
-	dir := t.TempDir()
-	jsonContent := `{
-	"name": "TestApp",
-	"version": "1.0"
-}`
-	err := os.WriteFile(filepath.Join(dir, configFileJson), []byte(jsonContent), 0644)
+func TestLoadEnvSuccessWithComments(t *testing.T) {
+	content := `APP_NAME=TestApp
+# This is a comment
+APP_VERSION=1.0
+`
+	createEnvFile(t, content)
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	err := config.LoadEnv()
 	assert.NoError(t, err)
-	var yamlCfg struct {
-		Name    string `json:"name"`
-		Version string `json:"version"`
-	}
-	err = NewConfig(&yamlCfg, "app", dir)
-	assert.NoError(t, err)
-	assert.Equal(t, "TestApp", yamlCfg.Name)
-	assert.Equal(t, "1.0", yamlCfg.Version)
+
+	assert.Equal(t, "TestApp", os.Getenv("APP_NAME"))
+	assert.Equal(t, "1.0", os.Getenv("APP_VERSION"))
+
+	_ = os.Unsetenv("APP_NAME")
+	_ = os.Unsetenv("APP_VERSION")
+	removeEnvFile(t)
 }
 
-func TestNewConfigSuccessReplaceEnvVariables(t *testing.T) {
-	err := os.Setenv("TESTAPPNAME", "goConfig")
-	require.NoError(t, err)
-	defer func() {
-		_ = os.Unsetenv("TESTAPPNAME")
-	}()
+func TestLoadEnvFailOpenDir(t *testing.T) {
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
 
-	content := `name: ${TESTAPPNAME}
-version: 1.0`
-	replacedContent, err := replaceEnvVariables(content)
-	assert.NoError(t, err)
-	assert.Equal(t, "name: goConfig\nversion: 1.0", replacedContent)
+	err := config.LoadEnv("nonexistent")
+	assert.Error(t, err)
 }
 
-func TestNewConfigErrorUnsupportedFileExtension(t *testing.T) {
+func TestLoadEnvFailMatchString(t *testing.T) {
+	content := `APP_NAME:=TestApp
+APP_VERSION=1.0
+`
+	createEnvFile(t, content)
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	err := config.LoadEnv()
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, goconfig.ErrInvalidEnvFormat)
+
+	removeEnvFile(t)
+}
+
+func TestLoadEnvFailSplitN(t *testing.T) {
+	content := `APP_NAME:TestApp
+APP_VERSION:1.0
+`
+	createEnvFile(t, content)
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	err := config.LoadEnv()
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, goconfig.ErrInvalidEnvFormat)
+
+	removeEnvFile(t)
+}
+
+func TestParseConfigSuccessYAML(t *testing.T) {
+	content := `App:
+  name: AppName
+  version: 1.0
+  log_level: 2
+storage:
+  master:
+    name: MASTER_CONNECTION
+    host: master-pg.localhost
+    port: 5432
+    user: user
+    password: password
+    database: db
+  slave:
+    name: SLAVE_CONNECTION
+    host: slave-pg.localhost
+    port: 5432
+    user: user
+    password: password
+    database: db
+`
+	dir, file := createConfigFile(t, content)
+
+	var yamlCfg AppConfig
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	err := config.ParseConfig(&yamlCfg, "App", dir)
+	assert.NoError(t, err)
+	assert.Equal(t, "AppName", yamlCfg.App.Name)
+	assert.Equal(t, "1.0", yamlCfg.App.Version)
+
+	_ = os.Remove(filepath.Join(dir, file))
+}
+
+func TestParseConfigSuccessWithEnvVariables(t *testing.T) {
+	err := os.Setenv("APP_NAME", "TestApp")
+	assert.NoError(t, err)
+
+	content := `App:
+  name: ${APP_NAME}
+  version: 1.0
+  log_level: 2
+storage:
+  master:
+    name: MASTER_CONNECTION
+    host: master-pg.localhost
+    port: 5432
+    user: user
+    password: password
+    database: db
+  slave:
+    name: SLAVE_CONNECTION
+    host: slave-pg.localhost
+    port: 5432
+    user: user
+    password: password
+    database: db
+`
+	dir, file := createConfigFile(t, content)
+
+	var yamlCfg AppConfig
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	err = config.ParseConfig(&yamlCfg, "App", dir)
+	assert.NoError(t, err)
+	assert.Equal(t, "TestApp", yamlCfg.App.Name)
+	assert.Equal(t, "1.0", yamlCfg.App.Version)
+
+	_ = os.Unsetenv("APP_NAME")
+	_ = os.Remove(filepath.Join(dir, file))
+}
+
+func TestParseConfigFailEnvNotFound(t *testing.T) {
+	content := `App:
+  name: ${APP_NAME}
+  version: 1.0
+  log_level: 2
+storage:
+  master:
+    name: MASTER_CONNECTION
+    host: master-pg.localhost
+    port: 5432
+    user: user
+    password: password
+    database: db
+  slave:
+    name: SLAVE_CONNECTION
+    host: slave-pg.localhost
+    port: 5432
+    user: user
+    password: password
+    database: db
+`
+	dir, file := createConfigFile(t, content)
+
+	var yamlCfg AppConfig
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	assert.PanicsWithError(t, "environment variable not found: APP_NAME", func() {
+		_ = config.ParseConfig(&yamlCfg, "App", dir)
+	})
+
+	_ = os.Remove(filepath.Join(dir, file))
+}
+
+func TestParseConfigFailUnsupportedFileExtension(t *testing.T) {
 	dir := t.TempDir()
 	unsupportedContent := `name: TestApp
 	version: 1.0`
-	err := os.WriteFile(filepath.Join(dir, "app.toml"), []byte(unsupportedContent), 0644)
+	err := os.WriteFile(filepath.Join(dir, "app.go"), []byte(unsupportedContent), 0644)
 	assert.NoError(t, err)
-	var yamlCfg struct {
-		Name    string `yaml:"name"`
-		Version string `yaml:"version"`
-	}
-	err = NewConfig(&yamlCfg, "app", dir)
+
+	var yamlCfg AppConfig
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	err = config.ParseConfig(&yamlCfg, "app", dir)
 	assert.Error(t, err)
 }
 
-func TestNewConfigErrorNoDirFound(t *testing.T) {
-	var yamlCfg struct {
-		Name    string `yaml:"name"`
-		Version string `yaml:"version"`
-	}
-	err := NewConfig(&yamlCfg, "app")
+func TestParseConfigFailNoDirFound(t *testing.T) {
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	var yamlCfg AppConfig
+	err := config.ParseConfig(&yamlCfg, "app")
 	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "error reading configuration: open config: no such file or directory")
+	assert.ErrorIs(t, err, goconfig.ErrOpenDir)
 }
 
-func TestNewConfigErrorMultipleDirNotFound(t *testing.T) {
-	var yamlCfg struct {
-		Name    string `yaml:"name"`
-		Version string `yaml:"version"`
-	}
-	err := NewConfig(&yamlCfg, "app", "configuration")
+func TestParseConfigFailMultipleDirNotFound(t *testing.T) {
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	var yamlCfg AppConfig
+	err := config.ParseConfig(&yamlCfg, "app", "configuration")
 	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "error reading configuration: "+
-		"open configuration: no such file or directory")
+	assert.ErrorIs(t, err, goconfig.ErrOpenDir)
 }
 
-func TestNewConfigErrorUnmarshallYAML(t *testing.T) {
+func TestParseConfigFailUnmarshall(t *testing.T) {
 	dir := t.TempDir()
 	content := `
 	name: TestApp
@@ -122,45 +256,31 @@ version: 1.0
 `
 	err := os.WriteFile(filepath.Join(dir, configFileYaml), []byte(content), 0644)
 	assert.NoError(t, err)
-	var yamlCfg struct {
-		Name    string `yaml:"name"`
-		Version string `yaml:"version"`
-	}
-	err = NewConfig(&yamlCfg, "app", dir)
+
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	var yamlCfg AppConfig
+	err = config.ParseConfig(&yamlCfg, "app", dir)
 	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "yaml: line 2: found character that cannot start any token")
+	assert.ErrorIs(t, err, goconfig.ErrUnmarshalling)
 }
 
-func TestNewConfigErrorUnmarshallJSON(t *testing.T) {
-	dir := t.TempDir()
-	jsonContent := `
-{
-	"name": "TestApp",}
-	"version": "1.0"
-}
-`
-	err := os.WriteFile(filepath.Join(dir, "app.json"), []byte(jsonContent), 0644)
-	assert.NoError(t, err)
-	var yamlCfg struct {
-		Name    string `json:"name"`
-		Version string `json:"version"`
-	}
-	err = NewConfig(&yamlCfg, "app", dir)
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "yaml: line 4: found character that cannot start any token")
-}
-
-func TestNewConfigErrorFileWithoutExtension(t *testing.T) {
+func TestParseConfigFailFileWithoutExtension(t *testing.T) {
 	dir := t.TempDir()
 	err := os.WriteFile(filepath.Join(dir, "appconfig"), []byte("dummy content"), 0644)
 	assert.NoError(t, err)
-	content, err := read("app", dir)
-	assert.Nil(t, content)
+
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	var yamlCfg AppConfig
+	err = config.ParseConfig(&yamlCfg, "app", dir)
 	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "unsupported extension app")
+	assert.ErrorIs(t, err, goconfig.ErrUnsupportedExt)
 }
 
-func TestNewConfigErrorReadingFile(t *testing.T) {
+func TestParseConfigFailReadingFile(t *testing.T) {
 	dir := t.TempDir()
 	err := os.WriteFile(filepath.Join(dir, configFileYaml), []byte("dummy content"), 0000)
 	assert.NoError(t, err)
@@ -169,131 +289,51 @@ func TestNewConfigErrorReadingFile(t *testing.T) {
 		_ = os.Chmod(name, mode)
 	}(filepath.Join(dir, configFileYaml), 0644)
 
-	content, err := read("app", dir)
-	assert.Nil(t, content)
+	config := goconfig.NewGoConfig()
+	assert.NotNil(t, config)
+
+	var yamlCfg AppConfig
+	err = config.ParseConfig(&yamlCfg, "app", dir)
 	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "error reading file")
 }
 
-func TestNewConfigErrorReplaceEnvVariablesPanicOnMissingEnvVar(t *testing.T) {
-	content := `name: ${TESTAPPNAME}
-version: 1.0`
-	assert.PanicsWithError(t, "environment variable TESTAPPNAME not found", func() {
-		_, _ = replaceEnvVariables(content)
-	})
-}
-
-func TestLoadEnvSuccess(t *testing.T) {
-	dir := "."
-	content := `APP_NAME=TestApp
-APP_VERSION=1.0
-`
-	err := os.WriteFile(filepath.Join(dir, ".env"), []byte(content), 0644)
-	assert.NoError(t, err)
-
-	err = LoadEnv()
-	assert.NoError(t, err)
-
-	assert.Equal(t, "TestApp", os.Getenv("APP_NAME"))
-	assert.Equal(t, "1.0", os.Getenv("APP_VERSION"))
-
-	_ = os.Unsetenv("APP_NAME")
-	_ = os.Unsetenv("APP_VERSION")
-	err = os.Remove(filepath.Join(dir, ".env"))
-	assert.NoError(t, err)
-}
-
-func TestLoadEnvSuccessWithComment(t *testing.T) {
-	dir := "."
-	content := `APP_NAME=TestApp
-# This is a comment
-APP_VERSION=1.0
-`
-	err := os.WriteFile(filepath.Join(dir, ".env"), []byte(content), 0644)
-	assert.NoError(t, err)
-
-	err = LoadEnv()
-	assert.NoError(t, err)
-
-	assert.Equal(t, "TestApp", os.Getenv("APP_NAME"))
-	assert.Equal(t, "1.0", os.Getenv("APP_VERSION"))
-
-	_ = os.Unsetenv("APP_NAME")
-	_ = os.Unsetenv("APP_VERSION")
-	err = os.Remove(filepath.Join(dir, ".env"))
-	assert.NoError(t, err)
-}
-
-func TestLoadEnvSuccessMultipleFiles(t *testing.T) {
-	dir := "."
-	content := `APP_NAME=TestApp
-APP_VERSION=1.0
-`
-	err := os.WriteFile(filepath.Join(dir, ".env"), []byte(content), 0644)
-	assert.NoError(t, err)
-
-	content = `AUTHOR=John Doe
-`
-
-	err = os.WriteFile(filepath.Join(dir, ".env2"), []byte(content), 0644)
-	assert.NoError(t, err)
-
-	err = LoadEnv(".env", ".env2")
-	assert.NoError(t, err)
-
-	assert.Equal(t, "TestApp", os.Getenv("APP_NAME"))
-	assert.Equal(t, "1.0", os.Getenv("APP_VERSION"))
-	assert.Equal(t, "John Doe", os.Getenv("AUTHOR"))
-
-	_ = os.Unsetenv("APP_NAME")
-	_ = os.Unsetenv("APP_VERSION")
-	_ = os.Unsetenv("AUTHOR")
-	err = os.Remove(filepath.Join(dir, ".env"))
-	assert.NoError(t, err)
-	err = os.Remove(filepath.Join(dir, ".env2"))
-	assert.NoError(t, err)
-}
-
-func TestLoadEnvErrorOpenFile(t *testing.T) {
-	err := LoadEnv("nonexistentfile")
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "open nonexistentfile: no such file or directory")
-}
-
-func TestLoadEnvErrorInvalidFormat(t *testing.T) {
-	dir := "."
-	content := `APP_NAME:TestApp
-APP_VERSION:1.0
-`
-	err := os.WriteFile(filepath.Join(dir, ".env"), []byte(content), 0644)
-	assert.NoError(t, err)
-
-	err = LoadEnv()
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "invalid .env format on line: APP_NAME:TestApp")
-
-	err = os.Remove(filepath.Join(dir, ".env"))
-	assert.NoError(t, err)
-}
-
-func TestSetEnvVarError(t *testing.T) {
-	originalSetenv := setEnvFunc
-	defer func() {
-		setEnvFunc = originalSetenv
-	}()
-
-	setEnvFunc = func(key, value string) error {
-		return fmt.Errorf("simulated error setting %s", key)
-	}
-
-	content := `APP_NAME=TestApp`
+func createEnvFile(t *testing.T, content string) {
 	dir := "."
 	err := os.WriteFile(filepath.Join(dir, ".env"), []byte(content), 0644)
-	require.NoError(t, err)
-
-	err = LoadEnv()
-	require.Error(t, err)
-
-	err = os.Remove(filepath.Join(dir, ".env"))
 	assert.NoError(t, err)
+}
+
+func removeEnvFile(t *testing.T) {
+	dir := "."
+	err := os.Remove(filepath.Join(dir, ".env"))
+	assert.NoError(t, err)
+}
+
+func createConfigFile(t *testing.T, content string) (string, string) {
+	dir := t.TempDir()
+	file := "App.yaml"
+	err := os.WriteFile(filepath.Join(dir, file), []byte(content), 0644)
+	assert.NoError(t, err)
+
+	return dir, file
+}
+
+type AppConfig struct {
+	App     App                `yaml:"App"`
+	Storage map[string]Storage `yaml:"storage"`
+}
+
+type App struct {
+	Name     string `yaml:"name"`
+	Version  string `yaml:"version"`
+	LogLevel string `yaml:"log_level"`
+}
+
+type Storage struct {
+	Name     string `yaml:"name"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Database string `yaml:"database"`
 }
